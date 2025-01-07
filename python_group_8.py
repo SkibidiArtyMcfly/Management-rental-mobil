42bfae864fc15a910e8ae3e6cd735e3269f29cb3
SkibidiArtyMcfly <arthur.alexander999@gmail.com>
2025-01-07 13:40:00 +0700

    Update python_group_8.py

--- python_group_8.py
+++ python_group_8.py
@@ -79,6 +79,6 @@ def gantikan_mobil_rusak():
 if st.button("View Mobil", type="primary"):
     st.table(df)
 if st.button("Refresh Table", type="primary"):
-    gantikan_mobilrusak()
+    gantikan_mobil_rusak()
     st.write("Table Refresed")
     
