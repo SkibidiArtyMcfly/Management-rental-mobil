76c1147af3a5d5f514355a3f2eafd0921de3bfff
SkibidiArtyMcfly <arthur.alexander999@gmail.com>
2025-01-08 18:20:33 +0700

    Update python_group_8.py

--- python_group_8.py
+++ python_group_8.py
@@ -25,6 +25,7 @@ data_mobil = [[1,  "Toyota Avanza",  1,  2,  "Jl. Thamrin No. 3, Jakarta Pusat",
      [8,  "Toyota Innova",  1,  4,  "Jl. Raya Cawang No. 10, Jakarta Timur",  104],
      [9,  "Honda HR-V",  2,  1,  "Jl. Raya Kebayoran Baru No. 5, Jakarta Selatan",  None],
      [10,  "Honda Jazz",  3,  4,  "Jl. Merdeka No. 15, Jakarta Barat",  None]]
+df = pd.DataFrame(data_mobil, columns=['ID Mobil', 'Nama Mobil', 'Tipe Mobil', 'Status', 'Lokasi', 'Customer ID'])
 
 lokasi_mapping = {
     1: "Jl. Raya Kebayoran Baru No. 5, Jakarta Selatan",
