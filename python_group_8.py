98644bd85ea28a609131b06cbd57d4f782cfdab3
SkibidiArtyMcfly <arthur.alexander999@gmail.com>
2025-01-08 10:09:32 +0700

    Update python_group_8.py

--- python_group_8.py
+++ python_group_8.py
@@ -81,6 +81,6 @@ st.title("Aplikasi Penggantian Mobil Rusak")
 
     # Tombol untuk menjalankan fungsi
 if st.button("Ganti Mobil Rusak"):
-    st.write(gantikan_mobil_rusak())
+    st.write()
 if st.button("View Mobil"):
     st.dataframe(df)
