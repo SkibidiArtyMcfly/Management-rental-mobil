5960a9783d311f7cebc7311df1cee154fd59b8c0
SkibidiArtyMcfly <arthur.alexander999@gmail.com>
2025-01-09 14:09:56 +0700

    Update python_group_8.py

--- python_group_8.py
+++ python_group_8.py
@@ -12,8 +12,7 @@ import numpy as np
 import pandas as pd
 import random
 
-if "boolean" not in st.session_state:
-    st.session_state.boolean == False
+st.session_state.boolean == False
 
 data_mobil = [[1,  "Toyota Avanza",  1,  2,  "Jl. Thamrin No. 3, Jakarta Pusat",  101],
      [2,  "Honda CR-V",  2,  3,  "Jl. Raya Cawang No. 10, Jakarta Timur",  102],
