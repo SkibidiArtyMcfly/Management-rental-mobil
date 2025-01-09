8dd4d6379a3530f904f895eb0d162e8fdd8e83d8
SkibidiArtyMcfly <arthur.alexander999@gmail.com>
2025-01-09 14:50:53 +0700

    Update python_group_8.py

--- python_group_8.py
+++ python_group_8.py
@@ -12,7 +12,10 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import random
-    
+
+if 'key' not in st.session_state:
+    st.session_state['key'] = 0
+
 data_mobil = [[1,  "Toyota Avanza",  1,  2,  "Jl. Thamrin No. 3, Jakarta Pusat",  101],
      [2,  "Honda CR-V",  2,  3,  "Jl. Raya Cawang No. 10, Jakarta Timur",  102],
      [3,  "Suzuki Ertiga",  1,  1,  "Jl. Raya Kebayoran Baru No. 5, Jakarta Selatan",  None],
