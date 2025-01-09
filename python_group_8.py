b9df74c07a5d1f8c12fe1e3f90f6782a521fffd7
SkibidiArtyMcfly <arthur.alexander999@gmail.com>
2025-01-09 14:10:08 +0700

    Update python_group_8.py

--- python_group_8.py
+++ python_group_8.py
@@ -12,7 +12,6 @@ import numpy as np
 import pandas as pd
 import random
 
-st.session_state.boolean == False
 
 data_mobil = [[1,  "Toyota Avanza",  1,  2,  "Jl. Thamrin No. 3, Jakarta Pusat",  101],
      [2,  "Honda CR-V",  2,  3,  "Jl. Raya Cawang No. 10, Jakarta Timur",  102],
