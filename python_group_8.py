95fff602cefaddf48d0eb555e4db099ba173f57d
SkibidiArtyMcfly <arthur.alexander999@gmail.com>
2025-01-09 15:08:08 +0700

    Update python_group_8.py

--- python_group_8.py
+++ python_group_8.py
@@ -17,7 +17,7 @@ if "boolean" not in st.session_state:
     st.session_state.boolean = False
 @st.cache(allow_output_mutation=True)
 def load_data():
-    datam_mobil = pd.read_csv('https://raw.githubusercontent.com/SkibidiArtyMcfly/Management-rental-mobil/refs/heads/main/data_mobil.csv')
+    data_mobil = pd.read_csv('https://raw.githubusercontent.com/SkibidiArtyMcfly/Management-rental-mobil/refs/heads/main/data_mobil.csv')
     return df
 '''data_mobil = [[1,  "Toyota Avanza",  1,  2,  "Jl. Thamrin No. 3, Jakarta Pusat",  101],
      [2,  "Honda CR-V",  2,  3,  "Jl. Raya Cawang No. 10, Jakarta Timur",  102],
