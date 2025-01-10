d661b3c6b9ec3ef644f8711a18b941915aa0588d
SkibidiArtyMcfly <arthur.alexander999@gmail.com>
2025-01-10 13:52:02 +0700

    Update python_group_8.py

--- python_group_8.py
+++ python_group_8.py
@@ -132,5 +132,5 @@ if st.session_state.boolean == False:
     st.table(df, column_config = myConfig, column_order=['ID_Mobil', 'Nama_Mobil','Tipe Mobil', 'Status_Mobil', 'Customer_ID'])
 else:
     gantikan_mobil_rusak()
-    st.dataframe(df, column_config = myConfig, column_order=['ID_Mobil', 'Nama_Mobil','Tipe Mobil', 'Status_Mobil', 'Customer_ID'])
+    st.table(df, column_config = myConfig, column_order=['ID_Mobil', 'Nama_Mobil','Tipe Mobil', 'Status_Mobil', 'Customer_ID'])
 
